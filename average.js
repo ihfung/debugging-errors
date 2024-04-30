340ed7eb5d5f03c41c198c561a6611a38f6dda84
ihleung <ihfungleung@gmail.com>
2024-04-30 16:57:44 -0400

    updated

--- average.js
+++ average.js
@@ -1,10 +1,13 @@
-function average(list) {
-    var sum = 0;
-  
-    for (var num of list) {
-      sum ++ num;
-    }
-  
-    return sum / list.length;
-  
+const average = function(list) {
+  //Using expression function instead of declaration function
+  let sum = 0; //Using var cause an error because we changing the value of sum
+
+  for (let num of list) {
+    //Using var cause an error because we changing the value of num
+
+    sum += num; //The ++ is not the correct operator to use here, it should be +=
+  }
+
+  return sum / list.length;
+}; //Missing closing bracket for the function call average
 console.log(average([3, 5, 7]));
--- reverse.js
+++ reverse.js
@@ -1,8 +1,8 @@
-var input = process.argv[2];
+let input = process.argv[2]; ////Using var cause an error because we changing of the input
 
-if (input)
-  console.log(reverse(input));
+const reverse = function(original) {
+  //Using expression function instead of declaration function
+  return original.split("").reverse().join(""); //There is no reverseList function and the original was a typo
+};
 
-function reverse(original) {
-  return orignal.split('').reverseList().join('');
-}
+if (input) console.log(reverse(input)); //Expression function must always be at the top of the scope
